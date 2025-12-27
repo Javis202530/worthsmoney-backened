221514bfe4961d38e1232546a1382e2ae138d877
Javis202530 <javisbbombo@gmail.com>
2025-12-27 14:19:17 +0300

    index.js

--- index.js
+++ index.js
@@ -101,6 +101,6 @@ app.get("/test-email", async (req, res) => {
   }
 });
 
+module.exports = app;
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
